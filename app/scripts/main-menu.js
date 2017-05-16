97d8fecf4caead2df63680d0440d3cbb9d2f4077
ksb <ksb@em70.ru>
2017-05-16 17:29:01 +0700

    base blocks

--- app/scripts/main-menu.js
+++ app/scripts/main-menu.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var x = document.getElementById('main-menu');
+function mainMenuClick() {
+    var x = document.getElementById('main-menu-wide');
     if (x.className.indexOf('em-show') == -1) {
         x.className += ' em-show';
     } else {
