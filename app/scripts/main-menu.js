ef5d629b136ff1c7a6eebd1defa67f87c86576c9
ksb <ksb@em70.ru>
2017-05-17 17:51:09 +0700

    dev

--- app/scripts/main-menu.js
+++ app/scripts/main-menu.js
@@ -1,5 +1,5 @@
 function mainMenuClick() {
-    var x = document.getElementById('main-menu-wide');
+    var x = document.getElementById('main-menu__mobile');
     if (x.className.indexOf('em-show') == -1) {
         x.className += ' em-show';
     } else {
