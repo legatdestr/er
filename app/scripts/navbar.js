34db799e7587eb7458d35b513461c31e3076a831
ksb <ksb@em70.ru>
2017-05-20 04:38:14 +0700

    2-nd variant

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -1,6 +1,6 @@
-EM.ui.mainMenu = {
+EM.ui.navbar = {
     click: function () {
-        var x = document.getElementById('main-menu__mobile');
+        var x = document.getElementById('navbar-small-screens');
         if (x.className.indexOf('em-show') == -1) {
             x.className += ' em-show';
         } else {
