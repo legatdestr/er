2c3cb1b517ddeee651350b2e49a47449fa9df733
rav <rav@em70.ru>
2017-06-05 17:52:30 +0700

    05/06/2017 changes

--- app/scripts/ps-widget.js
+++ app/scripts/ps-widget.js
@@ -1,130 +1,195 @@
-(function(global, extParams) {
-	var objProt = global.Object.prototype;
-
-	function main() {
-		var settings = {
+(function(global) {
+	var objProt = global.Object.prototype,
+		settings = {
 			psId: 'paid-services-widget',
 			formHeader: 'Поиск услуг',
 			searchInputLabel: 'Поисковый запрос',
 			searchPlaceholder: 'Поиск...',
-            advancedSearchId: 'advancedSearch',
-            searchFormId: 'searchForm',
-            psContentId: 'psContent'
+	        advancedSearchId: 'advancedSearch',
+	        searchFormId: 'searchForm',
+	        psContentId: 'psContent',
+	        psCount: 10
 		},
-        states = {
-            advancedSearch: false
-        };
+		advancedSearch;
 
-		function setupSettings(external, internal) {
-			var inter = internal || {},
-				prop;
+	function setupSettings(external, internal) {
+		var inter = internal || {},
+			prop;
 
-			if (objProt.toString.call(external) !== '[object Object]') {
-				return;
-			}
-			/* TODO: need to implement cases where properties are objects or array
-			of DOM elements */
-			for (prop in inter) {
-				if (objProt.hasOwnProperty.call(internal, prop) &&
-					typeof external[prop] !== 'undefined') {
-					internal[prop] = external[prop];
-				}
+		if (objProt.toString.call(external) !== '[object Object]') {
+			return;
+		}
+		/* TODO: need to implement cases where properties are objects or array
+		of DOM elements */
+		for (prop in inter) {
+			if (objProt.hasOwnProperty.call(internal, prop) &&
+				typeof external[prop] !== 'undefined') {
+				internal[prop] = external[prop];
 			}
 		}
-
-        function loadScript(url, callback) {
-            var head = document.getElementsByTagName('head')[0],
-                script = document.createElement('script');
-
-            script.src = url;
-
-            if (callback) {
-                //Humans browser
-                script.onload = callback;
-
-                //IE
-                script.onreadystatechange = function () {
-                    if (this.readyState == 'loaded' || this.readyState == 'complete') {
-                        callback();
-                    }
-                };
-            }
-            head.appendChild(script);
-        }
-
-		function renderTemplate(template, params, target) {
-			document.querySelector(target).innerHTML = template(params);
-		}
-
-        function showLoading() {
-            document.querySelector('.loading-overlay').classList.add('loading-overlay_show');
-        }
-
-        function hideLoading() {
-            document.querySelector('.loading-overlay').classList.remove('loading-overlay_show');
-        }
-
-        function runSearch() {
-            showLoading();
-            setTimeout(function() {
-                fetch('https://er.em70.ru/api/paidservices/find')
-                .then(function(response) {
-                    return response.json()
-                })
-                .then(function(response) {
-                    console.log(response);
-                    renderTemplate(
-                        EM.templates.services,
-                        {
-                            services: response.items
-                        },
-                        '#' + settings.psContentId
-                    )
-                    hideLoading();
-                });
-            }, 3000);
-        }
-
-        function resetSearchForm() {
-            document.getElementById(settings.searchFormId).reset();
-            document.getElementById(settings.psContentId).innerHTML = '';
-        }
-
-        function toggleAdvancedSearch() {
-            var advancedSearch = document.getElementById(settings.advancedSearchId);
-
-            if (advancedSearch) {
-                if (advancedSearch.classList.contains('advanced-search_show')) {
-                    advancedSearch.classList.remove('advanced-search_show');
-                    advancedSearch = false;
-                } else {
-                    advancedSearch.classList.add('advanced-search_show');
-                    advancedSearch = true;
-                }
-            }
-        }
-
-		setupSettings(extParams, settings);
-
-		renderTemplate(
-            EM.templates.main, 
-            {
-                formHeader: settings.formHeader,
-                searchPlaceholder: settings.searchPlaceholder,
-                searchInputLabel: settings.searchInputLabel,
-                advancedSearchId: settings.advancedSearchId,
-                categories: ['Диагностика', 'Анализы', 'Операция'],
-                lpus: ['Поликлиника 1', 'Поликлиника 4'],
-                searchFormId: settings.searchFormId,
-                psContentId: settings.psContentId
-            },
-            '#' + settings.psId
-        );
-
-        global.toggleAdvancedSearch = toggleAdvancedSearch;
-        global.runSearch = runSearch;
-        global.resetSearchForm = resetSearchForm;
 	}
 
-	document.addEventListener('DOMContentLoaded', main);
-}(window, ps_widget_params));
+	function renderTemplate(template, params, target) {
+		document.querySelector(target).innerHTML = template(params);
+	}
+
+    function showLoading() {
+        document.querySelector('.spinner').classList.add('spinner_show');
+    }
+
+    function hideLoading() {
+        document.querySelector('.spinner').classList.remove('spinner_show');
+    }
+
+    function validateSearchForm() {
+    	var result = false;
+
+    	if (document.getElementById('searchString').value !== '' ||
+    		(document.getElementById(settings.advancedSearchId).classList.contains('advanced-search_show') && (
+    			document.getElementById('categorySelect').value !== 'default' ||
+    			document.getElementById('lpuSelect').value !== 'default'
+    		))) {
+    		result = true;
+    	}
+
+    	return result;
+    }
+
+    function firstReq() {
+    	return new Promise(function(resolve, reject) {
+    		setTimeout(function() {
+    			resolve('{"lpus":[{"name":"Поликлиника 1","lpu":"0001"}],"paidcategories":[{"id":1,"name":"Анализы","parent_id":0}]}');
+    		}, 100);
+    	});
+    }
+
+    function secondReq() {
+    	return new Promise(function(resolve, reject) {
+    		setTimeout(function() {
+    			resolve('{"total":"100000","offset":"100","limit":"3","items":[{"service_name":"Услуга","service_price":"1000","service_desc":"описание услуги","lpu_name":"Поликлиника 1","lpu_ratio":"10.0","lpu_addresses":[{"name":"Отделение 1","address":"улица Пушкина"},{"name":"Отделение 2","address":"улица Колотушкина"}],"category_name":"Диагностика"},{"service_name":"Услуга №2","service_price":"1000","service_desc":"описание услуги","lpu_name":"Поликлиника 1","lpu_ratio":"10.0","lpu_addresses":[{"name":"Отделение 1","address":"улица Пушкина"},{"name":"Отделение 2","address":"улица Колотушкина"}],"category_name":"Диагностика"},{"service_name":"Услуга №3","service_price":"1000","service_desc":"описание услуги","lpu_name":"Поликлиника 1","lpu_ratio":"10.0","lpu_addresses":[{"name":"Отделение 1","address":"улица Пушкина"},{"name":"Отделение 2","address":"улица Колотушкина"}],"category_name":"Диагностика"}]}');
+    		}, 100);
+    	});
+    }
+
+    function runSearch() {
+    	if (!validateSearchForm()) {
+    		document.querySelector('.error-message').classList.add('error-message_active');
+    	} else {
+    		document.querySelector('.error-message').classList.remove('error-message_active');
+	        showLoading();
+            
+            //fetch('https://er.em70.ru/api/paidservices/find')
+            secondReq()
+            .then(function(response) {
+                //return response.json()
+                return JSON.parse(response);
+            })
+            .then(function(response) {
+                renderTemplate(
+                    EM.templates.services,
+                    {
+                        services: response.items
+                    },
+                    '#' + settings.psContentId
+                )
+                hideLoading();
+            });
+    	}
+    }
+
+    function resetSearchForm() {
+    	var resetEvent = new Event('clear-form');
+    	document.dispatchEvent(resetEvent);
+        document.getElementById(settings.searchFormId).reset();
+        document.getElementById(settings.psContentId).innerHTML = '';
+    }
+
+    function toggleAdvancedSearch() {
+        if (advancedSearch) {
+            if (advancedSearch.classList.contains('advanced-search_show')) {
+                advancedSearch.classList.remove('advanced-search_show');
+            } else {
+                advancedSearch.classList.add('advanced-search_show');
+            }
+        }
+    }
+
+	function run(params) {
+		setupSettings(params, settings);
+
+		//fetch('https://er.em70.ru/api/lpuspaidcategories/')
+		firstReq()
+        .then(function(response) {
+            //return response.json();
+            return JSON.parse(response)
+        })
+        .then(function(response) {
+            renderTemplate(
+	            EM.templates.main, 
+	            {
+	                formHeader: settings.formHeader,
+	                searchPlaceholder: settings.searchPlaceholder,
+	                searchInputLabel: settings.searchInputLabel,
+	                advancedSearchId: settings.advancedSearchId,
+	                categories: response.paidcategories,
+	                lpus: response.lpus,
+	                searchFormId: settings.searchFormId,
+	                psContentId: settings.psContentId
+	            },
+	            '#' + settings.psId
+	        );
+	        EM.initSelects();
+        	advancedSearch = document.getElementById(settings.advancedSearchId);
+        });
+	}
+
+	function createSearchObject() {
+		var searchString = document.getElementById('searchString'),
+			advancedSearch = document.getElementById(settings.advancedSearchId),
+			isAdvancedSearch = advancedSearch.classList.contains('advanced-search_show'),
+			sortedItems = document.querySelector('.ps-sorting__item_sorted'),
+			result = {},
+			i;
+
+		result.searching_string = searchString.value;
+		result.sorting_fields = [];
+		result.filtering_fields = [];
+
+		for (i = 0; i < sortedItems.length; i++) {
+			result.sorting_fileds[i] = {
+				'name': sortedItems[i].getAttribute('data-sort-field'),
+				'order_by': sortedItems[i].classList.contains('asc') ? 'asc' : 'desc'
+			}
+		}
+
+		if (isAdvancedSearch) {
+
+		}
+	}
+
+	function changeSortingButtonOrder(btn) {
+		if (!btn.classList.contains('ps-sorting__item_sorted')) {
+			btn.classList.add('ps-sorting__item_sorted', 'asc');
+		} else {
+			if (btn.classList.contains('asc')) {
+				btn.classList.remove('asc');
+				btn.classList.add('desc');
+			} else {
+				btn.classList.remove('desc');
+				btn.classList.add('asc');
+			}
+		}
+	}
+
+	function sort(sortingButton) {
+		changeSortingButtonOrder(sortingButton);
+	}
+
+	global.EM = (typeof EM === 'object' ? EM : window.EM = {});
+	EM.ps = {};
+	EM.ps.toggleAdvancedSearch = toggleAdvancedSearch;
+	EM.ps.runSearch = runSearch;
+	EM.ps.resetSearchForm = resetSearchForm;
+	EM.ps.sort = sort;
+	EM.ps.run = run;
+}(window));
--- app/scripts/select.js
+++ app/scripts/select.js
@@ -1,50 +1,101 @@
-/*
-Reference: http://jsfiddle.net/BB3JK/47/
-*/
-$( document ).ready(function() {
-$('select').each(function(){
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
-    $this.addClass('select-hidden'); 
-    $this.wrap('<div class="select"></div>');
-    $this.after('<div class="select-styled"></div>');
+(function(global) {
+    function initSelects() {
+        $('select').each(function(){
+            var $this = $(this), numberOfOptions = $(this).children('option').length;
+          
+            $this.addClass('select-hidden'); 
+            $this.wrap('<div class="select"></div>');
+            $this.after('<div class="select-styled"></div>');
 
-    var $styledSelect = $this.next('div.select-styled');
-    $styledSelect.text($this.children('option').eq(0).text());
-  
-    var $list = $('<ul />', {
-        'class': 'select-options'
-    }).insertAfter($styledSelect);
-  
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-    }
-  
-    var $listItems = $list.children('li');
-  
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function(){
-            $(this).removeClass('active').next('ul.select-options').hide();
+            var $styledSelect = $this.next('div.select-styled');
+            $styledSelect.text($this.children('option').eq(0).text());
+          
+            var $list = $('<ul />', {
+                'class': 'select-options'
+            }).insertAfter($styledSelect);
+          
+            for (var i = 0; i < numberOfOptions; i++) {
+                $('<li />', {
+                    text: $this.children('option').eq(i).text(),
+                    rel: $this.children('option').eq(i).val()
+                }).appendTo($list);
+            }
+          
+            var $listItems = $list.children('li');
+          
+            $styledSelect.click(function(e) {
+                e.stopPropagation();
+                $('div.select-styled.active').not(this).each(function(){
+                    $(this).removeClass('active').next('ul.select-options').hide();
+                });
+                $(this).toggleClass('active').next('ul.select-options').toggle();
+            });
+          
+            $listItems.click(function(e) {
+                e.stopPropagation();
+                $styledSelect.text($(this).text()).removeClass('active');
+                $this.val($(this).attr('rel'));
+                $list.hide();
+            });
+          
+            $(document).click(function() {
+                $styledSelect.removeClass('active');
+                $list.hide();
+            });
         });
-        $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-  
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
-  
-    $(document).click(function() {
-        $styledSelect.removeClass('active');
-        $list.hide();
+    }
+
+    document.addEventListener('clear-form', function() {
+        var defaultText = '--Не выбрано--';
+        document.getElementById('categorySelect').value = 'default';
+        document.getElementById('lpuSelect').value = 'default';
+
+        document.querySelector('.advanced-search-group_cat .select-styled').textContent = defaultText;
+        document.querySelector('.advanced-search-group_lpu .select-styled').textContent = defaultText;
     });
 
-});
-});
+/*    function initSelects() {
+        var selects = global.document.querySelectAll('select'),
+            i,
+            currentSelect,
+            numberOfOptions;
+
+        function wrap(wrapper, elems) {
+            if (!elms.length) elms = [elms];
+
+            // Loops backwards to prevent having to clone the wrapper on the
+            // first element (see `child` below).
+            for (var i = elms.length - 1; i >= 0; i--) {
+                var child = (i > 0) ? wrapper.cloneNode(true) : wrapper;
+                var el    = elms[i];
+
+                // Cache the current parent and sibling.
+                var parent  = el.parentNode;
+                var sibling = el.nextSibling;
+
+                // Wrap the element (is automatically removed from its current
+                // parent).
+                child.appendChild(el);
+
+                // If the element had a sibling, insert the wrapper before
+                // the sibling to maintain the HTML structure; otherwise, just
+                // append it to the parent.
+                if (sibling) {
+                    parent.insertBefore(child, sibling);
+                } else {
+                    parent.appendChild(child);
+                }
+            }
+        }
+
+        for (i = 0; i < selects.length; i++) {
+            currentSelect = selects[i];
+            numberOfOptions = currentSelect.querySelectAll('option').length;
+
+            currentSelect.classList.add('select-hidden');
+            wrap(document.createElement('div'), currentSelect);
+        }
+    }*/
+    global.EM = (typeof EM === 'object' ? EM : window.EM = {});
+    EM.initSelects = initSelects;
+})(window);
