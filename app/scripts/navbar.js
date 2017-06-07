c01cd7f5b81d8f1dbe54cde93223af484742177b
rav <rav@em70.ru>
2017-06-07 10:31:16 +0000

    free-doctor prototype

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -1,23 +1,10 @@
 EM.ui.navbar = {
     click: function () {
-        var x = document.getElementById('navbar-small-screens');
-        if (x.className.indexOf('em-show') == -1) {
-            x.className += ' em-show';
+        var mobileNav = document.querySelector('.main-header-mobile-menu__container');
+        if (mobileNav.className.indexOf('em-show') == -1) {
+            mobileNav.className += ' em-show';
         } else {
-            x.className = x.className.replace(' em-show', '');
+            mobileNav.className = mobileNav.className.replace(' em-show', '');
         }
     }
 };
-
-/**
- * Вносит поправку на padding хедера при переходе по якорю.. Иначе после кликка заголовок секции скрыт под меню
- */
-!(function () {
-    const menu = window.document.querySelector('#navbar-small-screens');
-    menu.addEventListener('click', function (e) {
-        setTimeout(function () {
-            let headerPaddingTop = 105, val = headerPaddingTop ;
-            window.scrollTo(0, window.scrollY - val);
-        }, 20);
-    });
-}());
