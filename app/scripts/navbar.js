03f1db5e6718988ff0872c90251f1ee0d58f0a9e
rav <rav@em70.ru>
2017-07-10 15:29:11 +0700

    v3 commit

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -1,10 +1,13 @@
 EM.ui.navbar = {
     click: function () {
-        var mobileNav = document.querySelector('.main-header-mobile-menu__container');
+        var mobileNav = document.querySelector('.main-header-mobile-menu__container'),
+        	toggleNav = document.querySelector('.icon-nav');
         if (mobileNav.className.indexOf('em-show') == -1) {
             mobileNav.className += ' em-show';
+            toggleNav.classList.add('open');
         } else {
             mobileNav.className = mobileNav.className.replace(' em-show', '');
+            toggleNav.classList.remove('open');
         }
     }
 };
